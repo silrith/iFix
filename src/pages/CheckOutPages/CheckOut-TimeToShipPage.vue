384a66729f0de52fae1ef451c9578a437dd086c5
wanudell <97380286+Wanudell@users.noreply.github.com>
2023-09-05 23:41:13 +0300

    v1.2.0

--- src/pages/CheckOutPages/CheckOut-TimeToShipPage.vue
+++ src/pages/CheckOutPages/CheckOut-TimeToShipPage.vue
@@ -2,12 +2,22 @@
   <div class="timeToShipDiv">
     <div class="checkOutHeaderDiv">
       <p class="checkOutHeader">All Set</p>
-      <p class="checkOutHeader">Time To
+      <p class="checkOutHeader">
+        Time To
         <span style="color: #f26d25"> Ship</span>
       </p>
       <p class="txt2" style="font-size: 15px; font-weight: 600">
         We emailed you a confirmation with your shipping label.
       </p>
+    </div>
+    <div class="mandatoryDisclosures">
+      <div class="mailSelectDiv"> //CSS EKLENECEK.
+        <div class="container-login102-form-btn">
+          <button class="btn btn-block py-2 btn-login" @click="">
+           Print Label
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -31,6 +41,18 @@
   font-family: Poppins-bold;
   font-size: 50px;
   line-height: 1;
+}
+
+.mandatoryDisclosures {
+  width: 50%;
+  height: 505px;
+  overflow-y: scroll;
+  background-image: url("@/assets/photos/deliveryTruck.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border: 2px solid black;
+  background-color: transparent;
 }
 
 @media (max-width: 772px) {
